b5665dcd9d5af9d6f09689883d36d2b216ee90cc
gjohl <gurpreetjohl@gmail.com>
2022-12-08 16:11:04 +0000

    Additional test for empty name in greeting function

--- tests/unit/test_main.py
+++ tests/unit/test_main.py
@@ -1,9 +1,17 @@
 import pytest
 
 from devopspractice.main import greeting
+from devopspractice.utils.exceptions import BadInputError
 
 
-@pytest.mark.parametrize("test_input,expected", [("Gurp", "Hello, Gurp"), ("Til", "Hello, Til"), ("Gurpreet Hisarli", "Hello, Gurpreet Hisarli")])
+@pytest.mark.parametrize("test_input,expected", [("Gurp", "Hello, Gurp"), ("Til", "Hello, Til"),
+                                                 ("Gurpreet Hisarli", "Hello, Gurpreet Hisarli")])
 def test_greeting(test_input, expected):
     assert greeting(test_input) == expected
+
+
+@pytest.mark.parametrize("test_input", [None, ""])
+def test_greeting_raises(test_input):
+    with pytest.raises(BadInputError, match="not be empty"):
+        greeting(test_input)
 
