f5ef791de62a5741f8a5f4c735ce868a29e0ddd3
gjohl <gurpreetjohl@gmail.com>
2022-12-08 16:36:32 +0000

    Refactor string_len tests

--- tests/unit/test_main.py
+++ tests/unit/test_main.py
@@ -18,12 +18,13 @@ def test_greeting_raises(test_input, error_message):
         greeting(test_input)
 
 
-@pytest.mark.parametrize('input_string,expected', [('Gurp', 4), ('Tilman', 6)])
-def test_string_len(input_string,expected):
-    assert string_len(input_string) == expected
+class TestStringLen:
+    @staticmethod
+    @pytest.mark.parametrize('input_string,expected', [('Gurp', 4), ('Tilman', 6)])
+    def test_expected_result(input_string, expected):
+        assert string_len(input_string) == expected
 
-
-@pytest.mark.parametrize('input', [1, 1., [], {'a': 1}])
-def test_string_len_raises(input):
-    with pytest.raises(BadInputError, match="must be a string"):
-        string_len(input)
+    @pytest.mark.parametrize('input_value', [1, 1., [], {'a': 1}])
+    def test_raises(self, input_value):
+        with pytest.raises(BadInputError, match="must be a string"):
+            string_len(input_value)
