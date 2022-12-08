dd4efa0d27b05ee713f57043a7bf0ed383091a9b
gjohl <gurpreetjohl@gmail.com>
2022-12-08 16:20:30 +0000

    final tests for greeting function

--- tests/unit/test_main.py
+++ tests/unit/test_main.py
@@ -10,8 +10,9 @@ def test_greeting(test_input, expected):
     assert greeting(test_input) == expected
 
 
-@pytest.mark.parametrize("test_input", [None, ""])
-def test_greeting_raises(test_input):
-    with pytest.raises(BadInputError, match="not be empty"):
+@pytest.mark.parametrize("test_input,error_message", [(None, "not be empty"), ("", "not be empty"),
+                                                      ("XO", "at least 3"), ("1", "at least 3")])
+def test_greeting_raises(test_input, error_message):
+    """Test that BadInputErrors are raised as expected."""
+    with pytest.raises(BadInputError, match=error_message):
         greeting(test_input)
-
