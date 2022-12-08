98a0994da207a60f9f95f610f0eaff8432a725ba
gjohl <gurpreetjohl@gmail.com>
2022-12-08 15:59:07 +0000

    Adding test for greeting function in main

--- tests/unit/test_main.py
+++ tests/unit/test_main.py
@@ -3,9 +3,7 @@ import pytest
 from devopspractice.main import greeting
 
 
-@pytest.mark.parametrize("test_input,expected", [("Gurp", "Hello, Gurp"), (None, "Hello, "), ("", 42)])
-def test_greeting():
-    actual = greeting("Gurp")
-    expected = "Hello, Gurp"
+@pytest.mark.parametrize("test_input,expected", [("Gurp", "Hello, Gurp"), ("Til", "Hello, Til"), ("Gurpreet Hisarli", "Hello, Gurpreet Hisarli")])
+def test_greeting(test_input, expected):
+    assert greeting(test_input) == expected
 
-    assert actual == expected
